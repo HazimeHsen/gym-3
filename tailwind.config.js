22e86cc8ef1fcf8beb916d11d3365f4488646541
Hsen-123 <hazimehussein01@gmail.com>
2023-12-18 15:05:28 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/Home.jsx", "./app/page.js", "./app/layout.js"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     fontFamily: {
       bebasNeue: ["Bebas Neue", "sans-serif"],
